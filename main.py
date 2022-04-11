b651122515d64aff37fc51f5b2d88c4eab19f0f1
sushi30 <26347589+sushi30@users.noreply.github.com>
2022-04-11 08:31:34 +0100

    added handling of edge case

--- main.py
+++ main.py
@@ -42,10 +42,16 @@ def ts2block(ts, debug=False):
     avg_mining_rate = (latest_block["time"] - GENESIS_BLOCK_TS) / latest_block["height"]
     if parent_block["time"] <= ts:
         return latest_block["height"]
+    visited = {latest_block["height"]}
+    next_block_to_check = latest_block["height"]
     while not parent_block["time"] <= ts < child_block["time"]:
-        delta = calculate_delta(ts, parent_block["time"], avg_mining_rate)
-        next_block_to_check = max(parent_block["height"] - delta, 0)
+        rate = 1
+        while next_block_to_check in visited:
+            rate /= 2
+            delta = calculate_delta(ts, parent_block["time"], avg_mining_rate / rate)
+            next_block_to_check = max(parent_block["height"] - delta, 0)
         parent_block = get_block(next_block_to_check)
+        visited.add(parent_block["height"])
         child_block = get_block(next_block_to_check + 1)
         calls += 2
         print_(f"{calls=},{parent_block['height']=},{child_block['height']=}")
